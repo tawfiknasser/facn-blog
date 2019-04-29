9f688289a63722bdac5032d7f244ad5dc1b891df
Tamer Nasser <tamernasser@outlook.com>
2019-04-29 17:36:23 +0300

    set port related #4

---
+++ src/app.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const favicon = require('serve-favicon');
+const bodyParser = require('body-parser');
+const compression = require('compression');
+
+// const controllers = require('./controllers');
+
+const app = express();
+
+app.set('port', process.env.PORT || 6666);
