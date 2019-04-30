7680afab80ecb3558c11ccf4ad90987a9bdc0d07
Tamer Nasser <tamernasser@outlook.com>
2019-04-30 15:05:19 +0300

    Added path for the views related #14

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const compression = require('compression');
 const exphbs = require("express-handlebars");
-
+const helpers = require("./views/helpers")
 const controllers = require('./controllers/index');
 
 const app = express();
@@ -16,6 +16,7 @@ app.use(favicon(path.join(__dirname, '..', 'assets', 'favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+app.set('views',path.join(__dirname, 'views'))
 app.set('view engine', 'hbs');
 app.engine(
   'hbs',
