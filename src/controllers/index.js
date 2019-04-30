08118265ef540445417d9b6e51f1aeed3e16aa3b
Tamer Nasser <tamernasser@outlook.com>
2019-04-30 15:05:46 +0300

    Added login post/get related #14

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const posts = require('./posts');
+const login = require('./login');
 const router = express.Router();
 
 router.get('/posts',posts.get);
+router.get('/',login.get)
+router.post('/login',login.post);
 
 module.exports = router;
