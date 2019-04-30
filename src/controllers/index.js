f6a43749616ee8e655373afedd590ba1b3ae99db
misterrodger <johndavidrodger@gmail.com>
2019-05-01 00:03:47 +0300

    Add signup.js to /controllers and to index.js (router)
    Relates#15

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const posts = require('./posts');
 const login = require('./login');
+const signup = require('./signup');
 const router = express.Router();
 
 router.get('/posts',posts.get);
@@ -8,5 +9,7 @@ router.post('/posts',posts.post);
 // router.post('/posts',posts.get);
 router.get('/',login.get)
 router.post('/login',login.post);
+router.get('/signup', signup.get);
+router.post('/signup', signup.post)
 
 module.exports = router;
