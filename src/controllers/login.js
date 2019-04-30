af61997548941a1a4667ef1ec6827b07a3838977
Tamer Nasser <tamernasser@outlook.com>
2019-04-30 15:31:51 +0300

    created login handler related #14

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,6 +1,18 @@
+const queries = require('../../database/queries/sql.js')
 exports.post = (req, res) => {
-  console.log(req.body);
-  res.redirect('/login');
+  if(queries.trueUser(req.body.username,req.body.password,(err,res)=>{
+    console.log(res);
+    if(err) {
+      console.log("Invalid")
+    }else{
+      if(res.rows[0].count>0){
+      console.log("Logged in");
+    }else{
+      console.log('invalid user');
+    }
+    }
+  }))
+  res.redirect('/posts');
 };
 
 exports.get = (req, res) => {
