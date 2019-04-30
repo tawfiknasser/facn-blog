98c98e8f68029e04ed837a00734cc2302d82858f
obaydah <obaydah@MBP.local>
2019-04-30 11:46:27 +0300

    add hbs engine related #7

--- src/app.js
+++ src/app.js
@@ -3,18 +3,33 @@ const path = require('path');
 const favicon = require('serve-favicon');
 const bodyParser = require('body-parser');
 const compression = require('compression');
+const exphbs = require("express-handlebars");
 
-// const controllers = require('./controllers');
+const controllers = require('./controllers/index');
 
 const app = express();
 
-app.set('port', process.env.PORT || 6666);
+app.set('port', process.env.PORT || 3001);
 app.disable('x-powered-by');
 app.use(compression());
-app.use(favicon(path.join(__dirname, '..', '..', 'public', 'assets', 'favicon.png')));
+app.use(favicon(path.join(__dirname, '..', 'assets', 'favicon.png')));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "views", "layouts")));
 app.use(controllers);
+
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "hbs");
+
+app.engine(
+    "hbs",
+    exphbs({
+        extname: "hbs",
+        layoutsDir: path.join(__dirname, "views", "layouts"),
+        partialsDir: path.join(__dirname, "views", "partials"),
+        defaultLayout: "main"
+    })
+);
 
 
 module.exports = app;
