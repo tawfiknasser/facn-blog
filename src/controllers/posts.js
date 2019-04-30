beb5692875386a7b9feea2d48cd0c2b72789d3b8
Tamer Nasser <tamernasser@outlook.com>
2019-04-30 23:43:59 +0300

    Revert "Add post"

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -10,23 +10,5 @@ exports.get = (req, res) => {
 }
 
 exports.post = (req, res) => {
-  let id = req.body.id;
-  let title = req.body.title;
-  let desc = req.body.desc;
-  queries.addNewPost(id,title,desc,likes,(err,result)=>{
-    if(err) console.log("ERROROROR");
-    res.redirect('/posts');
-  })
+  queries.addNewPost()
 }
-
-// exports.post = (req, res) => {
-//   console.log(req.body.name, req.body.image_url);
-//   res.redirect('/fruit');
-// };
-
-// const addNewPost = (writer_id, title, description, likes, cb) =>
-//   query.insert(
-//     "INSERT INTO user (writer_id,title,description,likes) VALUES ($1,$2,$3,$4);",
-//     [writer_id, title, description, likes],
-//     cb
-//   );
