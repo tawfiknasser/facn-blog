e9dfdf1193701a3f9a327ca5dbd01e99b9066d36
Tamer Nasser <tamernasser@outlook.com>
2019-04-30 16:11:58 +0300

    fixed redirect error related #14

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,19 +1,22 @@
 const queries = require('../../database/queries/sql.js')
 exports.post = (req, res) => {
-  if(queries.trueUser(req.body.username,req.body.password,(err,res)=>{
-    console.log(res);
-    if(err) {
-      console.log("Invalid")
-    }else{
-      if(res.rows[0].count>0){
-      console.log("Logged in");
-    }else{
-      console.log('invalid user');
-    }
-    }
-  }))
-  res.redirect('/posts');
-};
+  queries.trueUser(req.body.username, req.body.password, (err, resu) => {
+      console.log(res);
+      if (err) {
+        console.log("Invalid")
+      } else {
+        if (resu.rows[0].count > 0) {
+          console.log("Logged in");
+          res.redirect('/posts');
+        } else {
+          console.log('invalid user');
+          res.redirect('/login')
+        }
+      }
+    })
+}
+
+
 
 exports.get = (req, res) => {
   res.render('login');
