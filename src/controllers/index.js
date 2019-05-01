87c72fc5c650cc2bf6b306d6c801235664e4b0ba
Tamer Nasser <tamernasser@outlook.com>
2019-05-01 12:16:16 +0300

    Fixed linter errors

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,14 +1,15 @@
-const express = require("express");
-const posts = require("./posts");
-const login = require("./login");
+const express = require('express');
+const posts = require('./posts');
+const login = require('./login');
+
 const router = express.Router();
 
-router.get("/", login.get);
-router.post("/login", login.post);
+router.get('/', login.get);
+router.post('/login', login.post);
 
-router.get("/posts", posts.get);
-router.post("/posts", posts.addPost);
-router.post("/editPost", posts.editPost);
-router.post("/deleteBlog", posts.deleteBlog);
+router.get('/posts', posts.get);
+router.post('/posts', posts.addPost);
+router.post('/editPost', posts.editPost);
+router.post('/deleteBlog', posts.deleteBlog);
 
 module.exports = router;
