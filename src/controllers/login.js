1e630a0bbdd1345f412f6cc0a0e3815b8715ac24
Tamer Nasser <tamernasser@outlook.com>
2019-05-01 14:07:18 +0300

    added cookies

--- src/controllers/login.js
+++ src/controllers/login.js
@@ -1,13 +1,26 @@
+const ppcookie = require('cookie');
+const { sign, verify } = require('jsonwebtoken');
 const queries = require('../../database/queries/sql.js');
+const utils = require('../utils/utils');
+
+const secret = 'blablabalbsasfsafe';
 
 exports.post = (req, res) => {
   queries.trueUser(req.body.username, req.body.password, (err, resu) => {
     console.log(res);
     if (err) {
-      console.log('Invalid');
+      console.log('Error in validating the username and password');
     } else {
       if (resu.rows[0].count > 0) {
-        console.log('Logged in');
+        const uDetails = {
+          'content-type': 'application/json',
+          uUser: req.body.username
+        };
+        const cookie = sign(uDetails, secret);
+        const options = {
+          httpOnly: true
+        };
+        res.cookie('udetails', cookie, options);
         res.redirect('/posts');
       } else {
         console.log('invalid user');
@@ -16,6 +29,47 @@ exports.post = (req, res) => {
     }
   });
 };
+
+exports.checkauth = (res, req) => {
+  if (!req.headers.cookie) res.render('login');
+  else {
+    let jwt;
+    try {
+      jwt = ppcookie.parse(req.headers.cookie);
+    } catch (error) {
+      res.render('login');
+    }
+
+    if (jwt) {
+      verify(jwt.udetails, secret, (err, jwt) => {
+        if (err) res.render('login');
+
+        const { uUser } = jwt;
+        queries.getUserPass(uUser, (err, resultsss) => {
+          if (err) console.log(err);
+          else {
+            if (resultsss.rowCount === 0) res.render('login');
+            else if (resultsss.rowCount === 1) {
+              utils.comparePasswords(
+                req.body.password,
+                resultsss.rows[0].password,
+                (err, success) => {
+                  if (err) res.render('login');
+                  else {
+                    if (success) res.render('posts');
+                    else res.render('login');
+                  }
+                }
+              );
+            }
+          }
+        });
+      });
+    }
+  }
+};
+
+
 exports.get = (req, res) => {
   res.render('login');
 };
