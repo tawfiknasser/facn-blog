49bca30ac5c4ef15a216bb8899ead235bb7651f9
obaydah <obaydah@MBP.local>
2019-05-01 11:37:37 +0300

    added like route related #19

--- src/controllers/index.js
+++ src/controllers/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const posts = require('./posts');
 const login = require('./login');
+const like = require('./like');
 const router = express.Router();
 
 router.get('/posts',posts.get);
@@ -8,5 +9,6 @@ router.post('/posts',posts.post);
 // router.post('/posts',posts.get);
 router.get('/',login.get)
 router.post('/login',login.post);
+router.post('/like',like.post);
 
 module.exports = router;
