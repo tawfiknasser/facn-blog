7f2085328d2cdbc69574b40a40122c58599e2589
majdya <majd.ya.1995@gmail.com>
2019-05-02 13:35:58 +0300

    resolve conflict

--- src/controllers/posts.js
+++ src/controllers/posts.js
@@ -1,34 +1,50 @@
+const ppcookie = require("cookie");
+const { decode } = require("jsonwebtoken");
 const queries = require("../database/queries/sql");
 
 exports.get = (req, res) => {
   queries.selectAllFrom("blogs", (err, result) => {
     if (err) console.log("ERRROR");
-    let userId = 12;
-    res.render("posts", {
-      blogs: result.rows
-        .sort(function(a, b) {
-          return a.id - b.id;
-        })
-        .map((blog) => {
-          if (blog.writer_id === userId) blog.isOwner = true;
-          else blog.isOwner = false;
-          return blog;
-        })
+
+    let decoded = decode(ppcookie.parse(req.headers.cookie).udetails);
+
+    queries.selectUserByUserName(decoded.uUser, (err, resu) => {
+      if (err) console.log(err);
+      let user = resu.rows[0];
+
+      res.render("posts", {
+        blogs: result.rows
+          .sort(function(a, b) {
+            return a.id - b.id;
+          })
+          .map(blog => {
+            if (blog.writer_id === user.id) blog.isOwner = true;
+            else blog.isOwner = false;
+            return blog;
+          }),
+        user: user.name
+      });
     });
   });
 };
 
 exports.addPostGet = (req, res) => {
-  res.render('addpost');
+  res.render("addpost");
 };
 
 exports.addPost = (req, res) => {
-  const id = req.body.id;
-  const title = req.body.title;
-  const desc = req.body.desc;
-  queries.addNewPost(id, title, desc, (err, result) => {
+  let decoded = decode(ppcookie.parse(req.headers.cookie).udetails);
+  queries.selectUserByUserName(decoded.uUser, (err, resu) => {
     if (err) console.log(err);
-    res.redirect("/posts");
+    let user = resu.rows[0];
+
+    const id = user.id;
+    const title = req.body.title;
+    const desc = req.body.desc;
+    queries.addNewPost(id, title, desc, (err, result) => {
+      if (err) console.log(err);
+      res.redirect("/posts");
+    });
   });
 };
 
